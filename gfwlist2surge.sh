35f59c0fdad6d28775d732bd8971ee67238c89b0
zhenhappy <q505507538@gmail.com>
2020-01-20 11:27:44 +0800

    first commit

--- gfwlist2surge.sh
+++ gfwlist2surge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 curl -O https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt
 curl -O https://raw.githubusercontent.com/gfwlist/tinylist/master/tinylist.txt
-python gfwlist2surge.py -i gfwlist.txt -o gfwlist.list
-python gfwlist2surge.py -i tinylist.txt -o tinylist.list
+python3 gfwlist2surge.py -i gfwlist.txt -o gfwlist.list
+python3 gfwlist2surge.py -i tinylist.txt -o tinylist.list
 git add .
 git commit -m "update"
 git push -u origin master
